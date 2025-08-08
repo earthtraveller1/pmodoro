fef29a7db421ac917bf759de403ce3f6d63254f1
earthtraveller1 <liangyuezhao7@gmail.com>
2025-08-08 01:09:12 -0400

    Abstracted some of the text rendering away and put them into their own functions.

--- src/main.c
+++ src/main.c
@@ -7,12 +7,27 @@
 #define HEIGHT 400
 
 #define TIME_FONT_SIZE 100
-#define TIME_TOP_PADDING 50
+#define LABEL_FONT_SIZE 25
+
+#define TIME_TOP_PADDING 60
+#define LABEL_BOTTOM_PADDING 40
+
+const Color FONT_COLOR = { 
+    .r = 200, 
+    .g = 255, 
+    .b = 200, 
+    .a = 255
+};
 
 struct time {
     int minutes;
     int seconds;
 };
+
+void draw_centered_text(const char* text, int ypos, int font_size) {
+    int text_width = MeasureText(text, font_size);
+    DrawText(text, (WIDTH - text_width) / 2, ypos, font_size, FONT_COLOR);
+}
 
 int main(void) {
     SetConfigFlags(FLAG_WINDOW_HIGHDPI);
@@ -29,25 +44,15 @@ int main(void) {
         char text_buf[512];
         int text_width;
 
+        draw_centered_text("Work", TIME_TOP_PADDING - LABEL_BOTTOM_PADDING, LABEL_FONT_SIZE);
+
         snprintf(text_buf, 512, "%02d:%02d", work.minutes, work.seconds);
-        text_width = MeasureText(text_buf, TIME_FONT_SIZE);
-        DrawText(
-            text_buf, 
-            (WIDTH - text_width) / 2, 
-            TIME_TOP_PADDING, 
-            TIME_FONT_SIZE, 
-            (Color){ .r = 200, .g = 255, .b = 200, .a = 255}
-        );
+        draw_centered_text(text_buf, TIME_TOP_PADDING, TIME_FONT_SIZE);
+
+        draw_centered_text("Rest", TIME_TOP_PADDING * 2 + TIME_FONT_SIZE - LABEL_BOTTOM_PADDING, LABEL_FONT_SIZE);
 
         snprintf(text_buf, 512, "%02d:%02d", rest.minutes, rest.seconds);
-        text_width = MeasureText(text_buf, TIME_FONT_SIZE);
-        DrawText(
-            text_buf, 
-            (WIDTH - text_width) / 2, 
-            TIME_TOP_PADDING * 2 + TIME_FONT_SIZE, 
-            TIME_FONT_SIZE, 
-            (Color){ .r = 200, .g = 255, .b = 200, .a = 255}
-        );
+        draw_centered_text(text_buf, TIME_TOP_PADDING * 2 + TIME_FONT_SIZE, TIME_FONT_SIZE);
 
         EndDrawing();
     }
