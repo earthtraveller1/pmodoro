2bc6b00ff9d21b68423ee1d29ea2ed890a349c6e
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2025-08-14 12:11:17 -0400

    Added a macro to quickly create a vector2.

--- src/vector-math.h
+++ src/vector-math.h
@@ -1,3 +1,5 @@
 #pragma once
 
-#define VEC_ADD(a, b) (Vector2) { (a).x + (b).x, (a).y + (b).y }
+#define VEC(x, y) ((Vector2) { (x), (y) })
+
+#define VEC_ADD(a, b) ((Vector2) { (a).x + (b).x, (a).y + (b).y })
