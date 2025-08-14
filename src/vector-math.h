763884317cc01d1256cd74b2ffabd401b6560168
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2025-08-14 13:04:25 -0400

    Used more vanilla header guards to look cool.

--- src/vector-math.h
+++ src/vector-math.h
@@ -1,5 +1,8 @@
-#pragma once
+#ifndef INCLUDED_vector_math_h
+#define INCLUDED_vector_math_h
 
 #define VEC(x, y) ((Vector2) { (x), (y) })
 
 #define VEC_ADD(a, b) ((Vector2) { (a).x + (b).x, (a).y + (b).y })
+
+#endif
