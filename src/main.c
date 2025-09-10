a9c8925e0a0ee3bb97639be329dd19934fda385e
Liangyue Zhao <liangyuezhao7@gmail.com>
2025-09-10 09:17:22 -0400

    Created the basic timer page.

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <raylib.h>
 
 #include <stdbool.h>
+#include <string.h>
+#include <assert.h>
 
 #include "vector-math.h"
 #include "buttons.h"
@@ -88,6 +90,39 @@ void update_main_page(struct main_page* main_page, struct time* work, struct tim
     EndDrawing();
 }
 
+struct timer_page {
+    struct button stop_button;
+    double start_time;
+};
+
+struct timer_page new_timer_page(void) {
+    struct timer_page timer_page;
+    memset(&timer_page, 0, sizeof(timer_page));
+
+    Vector2 button_size = VEC(150.0f, 50.0f);
+
+    timer_page.stop_button = (struct button) {
+        .pos = VEC(( button_size.x - WIDTH ) / 2, HEIGHT - 100.0),
+        .size = button_size,
+        .color = BUTTON_COLOR_2,
+        .is_triangle = false,
+        .is_upside_down = false,
+        .label = "Stop"
+    };
+
+    return timer_page;
+}
+
+void update_timer_page(struct timer_page* timer_page, struct time work, struct time rest) {
+    assert(page == pages_rest || page == pages_work);
+
+    BeginDrawing();
+
+    draw_button(&timer_page->stop_button);
+
+    EndDrawing();
+}
+
 void draw_centered_text(const char* text, int ypos, int font_size) {
     int text_width = MeasureText(text, font_size);
     Vector2 position = VEC((WIDTH - text_width) / 2.0, ypos);
@@ -104,9 +139,14 @@ int main(void) {
     struct time rest = {5, 0};
 
     struct main_page main_page = new_main_page();
+    struct timer_page timer_page = new_timer_page();
 
     while (!WindowShouldClose()) {
-        update_main_page(&main_page, &work, &rest);
+        if (page == pages_main) {
+            update_main_page(&main_page, &work, &rest);
+        } else if (page == pages_rest || page == pages_work) {
+            update_timer_page(&timer_page, work, rest);
+        }
     }
 
     CloseWindow();
